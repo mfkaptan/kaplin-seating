47bbb462ffa912295390aed9393c45943b6ad0f2
mfkaptan <kaptanmf@gmail.com>
2017-03-11 23:55:40 +0100

    Update init db

--- db/init_db.js
+++ db/init_db.js
@@ -1,7 +1,7 @@
 const Datastore = require('nedb');
 const data = require('./guests.json');
-const db = new Datastore({ filename: 'guests.db', autoload: true });
+const db = new Datastore({ filename: 'db/guests.db', autoload: true });
 
-db.insert(data, function (err, newDocs) {
+db.insert(data, function(err, newDocs) {
     console.log(newDocs.length + " guests loaded successfully.");
 });
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var path = require('path');
 const Datastore = require('nedb');
 let db = new Datastore({ filename: 'db/guests.db', autoload: true });
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:8000
 app.use('/', express.static(__dirname + '/public'));
 app.listen(8000, serve);
 
 function serve() {
-    db.find({}, function (err, docs) {
+    db.find({}, function(err, docs) {
         console.log(docs)
     });
 
