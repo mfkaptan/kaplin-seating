2d156e792cf0f0c3c0572ff6d01c5833a58eec10
mfkaptan <kaptanmf@gmail.com>
2017-03-16 21:56:11 +0100

    Add badges

--- public/js/html.js
+++ public/js/html.js
@@ -1,12 +1,21 @@
 function appendSidebar(divId, guests) {
   $(divId).empty();
 
-  const LI = '<a id="ID" href="#" class="list-group-item">NAME</a>';
-  const N = "NAME";
-  const ID = "ID";
+  const LI = '<a id="_ID_" href="#" class="list-group-item">_NAME__SPAN_</a>';
+  const SPAN = '<span class="badge">_SPAN_</span>';
+  const N = "_NAME_";
+  const ID = "_ID_";
+  const S = "_SPAN_";
 
   if (guests)
     guests.forEach(function(g) {
-      $(divId).append(LI.replace(N, g.name).replace(ID, g.id));
+      let a = LI.replace(N, g.name).replace(ID, g.id);
+      let badge = "";
+
+      if (g.table)
+        badge = SPAN.replace(S, g.table);
+
+      a = a.replace(S, badge);
+      $(divId).append(a);
     })
 }
