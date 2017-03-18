aac2cac3f5a0961b6fd8abaa0b9342b51011a543
mfkaptan <kaptanmf@gmail.com>
2017-03-18 16:46:37 +0100

    Card-like sidebar elements

--- public/js/html.js
+++ public/js/html.js
@@ -1,9 +1,11 @@
 function appendSidebar(divId, guests) {
   $(divId).empty();
 
-  const LI = '<a id="_ID_" href="#" class="list-group-item">_NAME__SIZE__TABLE_</a>';
-  const TABLE = '<label class="label label-success pull-right">M._TABLE_</label>';
-  const SIZE = '<label class="label label-info pull-right">x_SIZE_</label>';
+  const LI = '<a id="_ID_" href="#" class="list-group-item"> \
+               <h5 class="list-group-item-heading">_NAME_</h5>\
+               <p class="list-group-item-text">_TABLE__SIZE_</p></a>';
+  const TABLE = '<label class="label label-success">Masa _TABLE_</label>';
+  const SIZE = '<label class="label label-info pull-right">_SIZE_ kişi</label>';
   const ID = "_ID_";
   const N = "_NAME_";
   const T = "_TABLE_";
@@ -11,12 +13,12 @@ function appendSidebar(divId, guests) {
 
   if (guests)
     guests.forEach(function(g) {
-      let tableLabel = "";
+      let tableLabel = "-";
       if (g.table)
         tableLabel = TABLE.replace(T, g.table);
 
       let sizeBadge = SIZE.replace(S, g.size);
-      let a = LI.replace(N, g.name).replace(ID, g.id).replace(S, sizeBadge).replace(T, tableLabel);
+      let a = LI.replace(N, g.name).replace(ID, g.id).replace(T, tableLabel).replace(S, sizeBadge);
       $(divId).append(a);
     })
 }
